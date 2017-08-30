8c1e321a3c55e1e891bbaa1cd82b316a34b400d5
Dan <dcallinder1s@gmail.com>
2017-08-30 02:17:05 +0000

    inital commit

---
+++ lesson2/3.rb
@@ -0,0 +1,31 @@
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+  
+  def name=(full_name)
+      parts = full_name.split
+      @first_name = parts.first
+      @last_name = parts.size > 1 ? parts.last : ''
+  end
+end
+
+
+bob = Person.new('Robert')
+bob.name                  # => 'Robert'
+bob.first_name            # => 'Robert'
+bob.last_name             # => ''
+bob.last_name = 'Smith'
+bob.name                  # => 'Robert Smith'
+
+bob.name = "John Adams"
+p bob.first_name            # => 'John'
+bob.last_name   
---
+++ lesson3/2.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Pets
+  def speak
+    'bark!'
+  end
+
+  def run
+    'running!'
+  end
+
+  def jump
+    'jumping!'
+  end
+end
+  
+class Dog < Pets
+
+  def swim
+    'swimming!'
+  end
+
+  def fetch
+    'fetching!'
+  end
+end
+
+class Cat < Pets 
+  def speak
+    'meow'
+  end
+end
+  
+kitty = Cat.new
+p kitty.speak
+
+frank = Dog.new
+binding pry
+p frank.speak
+
+# Pets
+# Dog - Cat
