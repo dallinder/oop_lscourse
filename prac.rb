e740539b9080b6d00721acb7b3d61a74aa39400e
Dan <dcallinder1s@gmail.com>
2017-09-02 22:44:44 +0000

    just a practice file

--- prac.rb
+++ prac.rb
@@ -1,8 +1,26 @@
-array = ['rock', 'paper', 'scissors']
+module Describable
+  def describe_shape
+    "I am a #{self.class} and have #{self.class::SIDES} sides."
+  end
+end
 
-p array - ['rock']
+class Shape
+  include Describable
 
-p array
+  def self.sides
+    self::SIDES
+  end
+end
 
+class Quadrilateral < Shape
+  def sides
+    SIDES
+  end
+  SIDES = 4
+end
 
+class Square < Quadrilateral; end
 
+p Square.sides # => 4
+p Square.new.sides # => 4
+p Square.new.describe_shape # => "I am a Square and have 4 sides."
